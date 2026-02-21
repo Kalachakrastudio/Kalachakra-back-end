582c1543fd7d69d0871dc04f685cbe9723102d06
Kalachakrastudio <kalachakrastudio@gmail.com>
2026-02-21 23:21:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 const hamburger = document.getElementById("hamburger");
 const nav = document.getElementById("nav");
 
-hamburger.addEventListener("click", () => {
+hamburger.addEventListener("click", function(){
     hamburger.classList.toggle("active");
     nav.classList.toggle("active");
 });
 
 document.querySelectorAll(".nav a").forEach(link => {
-    link.addEventListener("click", () => {
+    link.addEventListener("click", function(){
         hamburger.classList.remove("active");
         nav.classList.remove("active");
+    });
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".hero-title span", {
+    y:120,
+    opacity:0,
+    stagger:0.08,
+    duration:1,
+    ease:"power4.out"
+});
+
+gsap.utils.toArray(".service-card").forEach(card=>{
+    gsap.from(card,{
+        scrollTrigger:{
+            trigger:card,
+            start:"top 85%"
+        },
+        y:80,
+        opacity:0,
+        duration:1,
+        ease:"power3.out"
     });
 });
